1f1ae0c759de28b9672dbb5fc73599e455963689
rafiki <mark@rafiki>
2015-10-14 12:06:50 +0200

    updates to pypi setup files

--- setup.py
+++ setup.py
@@ -1,24 +1,50 @@
 # -*- coding: utf-8 -*-
 
 """
-	for installing with pip
+	Adapted from https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
 """
 
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup
+
+
+with open('README.rst', 'r') as fh:
+	readme = fh.read()
+
 
 setup(
-	name='django_hreflang',
-	version='v1.4',
-	author='Mark V',
-	author_email='mdilligaf@gmail.com',
-	packages=find_packages(),
-	include_package_data=True,
+	name='django_hreflang',  # should have used a - sorry
+	description='Generate the hreflang html header lines when using i18n urls on Django sites',
+	long_description=readme,
 	url='https://bitbucket.org/mverleg/django_hreflang',
+	author='Mark V',
+	maintainer='(the author)',
+	author_email='mdilligaf@gmail.com',
 	license='Revised BSD License (LICENSE.txt)',
-	description='Generate the hreflang html header lines when using i18n urls',
-	zip_safe=True,
-	install_requires = [
+	keywords=['django', 'hreflang', 'http-headers', 'internationalization'],
+	version='1.6',
+	packages=['hreflang', 'hreflang.templatetags'],
+	include_package_data=True,
+	zip_safe=False,
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+	install_requires=[
 		'django',
 	],
 )
+
+
